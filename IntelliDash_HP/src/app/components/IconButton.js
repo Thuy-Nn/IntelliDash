e643d37e3ddfdc9d46ab397e1ad75a7cb53fd12b
ThuyNguyen-Umbrella <thuy.nguyen@umbrellaconsulting.de>
2026-02-03 18:31:27 +0100

    reorganized files

--- IntelliDash_HP/src/app/components/IconButton.js
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/IconButton.module.css';
-
-export default function IconButton({ iconClass, label, transparent = false, className = "", ...props }) {
-    return <button {...props}
-        className={styles.buttonOuter + (transparent ? "" : " " + styles.buttonOuter__withBorder) + " " + className}>
-        {iconClass && <span className={iconClass} />}
-        {label}
-    </button>
-}
--- IntelliDash_HP/src/components/charts/SingleValueChart.js
+++ IntelliDash_HP/src/components/charts/SingleValueChart.js
@@ -1,12 +1,14 @@
+import styles from '@/styles/SingleValueChart.module.css'
+
 export default function SingleValueChart({ valueData }) {
   if (!valueData || valueData.value === null || valueData.value === undefined) return null
 
   const { value, unit, position } = valueData
-  return <div className="text-6xl overflow-hidden whitespace-nowrap font-bold flex items-center justify-center h-full pb-8">
+  return <div className={styles.container}>
     <div>
-      {position === 'prefix' && unit ? <span className="mr-2 text-4xl opacity-50 align-baseline">{unit}</span> : null}
+      {position === 'prefix' && unit ? <span className={`${styles.unit} ${styles.unitPrefix}`}>{unit}</span> : null}
       {value.toLocaleString()}
-      {position !== 'prefix' && unit ? <span className="ml-2 text-4xl opacity-50 align-baseline">{unit}</span> : null}
+      {position !== 'prefix' && unit ? <span className={`${styles.unit} ${styles.unitSuffix}`}>{unit}</span> : null}
     </div>
   </div>
 }
